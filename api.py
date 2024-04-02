d05059044b1b1ae7b2870ca40de52b89fc7e6763
Leonardo Dreizzen <64935571+leodreizzen@users.noreply.github.com>
2024-04-02 17:07:22 -0300

    Update api.py

--- api.py
+++ api.py
@@ -15,7 +15,3 @@ async def root():
     session_event.load()
 
     return {"message": str(session_event.laps.pick_fastest().get_telemetry())}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
